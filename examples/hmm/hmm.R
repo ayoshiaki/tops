916aaedbd04ba1c9e888b6c4a897d4446651d4ec
Andre Yoshiaki Kashiwabara <akashiwabara@usp.br>
2012-07-18 13:27:04 -0300

    new examples was included

--- examples/hmm/hmm.R
+++ examples/hmm/hmm.R
@@ -1,3 +1,5 @@
+# Here, we are using the HiddenMarkov package to validate our BaumWelch implementation. The estimated parameters  must be the same in ToPS and HiddenMarkov implementations.
+
 library(HiddenMarkov)
 
 source('sequence_from_hmm.R')
@@ -5,7 +7,8 @@ source('sequence_from_hmm.R')
 Pi <- matrix ( c(0.5,0.5,0.5,0.5) , ncol=2 )
 delta <- c(0.5,0.5)
 
-hmm <- dthmm(sequence, Pi, delta, "binom", pm = list(prob=c(0.3, 0.1)) , pn = list(size=rep(1, 10000)), discrete = True)
+hmm <- dthmm(sequence, Pi, delta, "binom", pm = list(prob=c(0.3, 0.1)) , pn = list(size=rep(1, 10000)), discrete = 1)
 
 hmm_trained <- BaumWelch(hmm)
 
+hmm_trained$pm
